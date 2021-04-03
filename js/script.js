91958b4d808ad467a66e6164635adb5a6d727f93
Vladimir <pustovetov73@gmail.com>
2021-04-03 17:28:06 +0300

    one save

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use sctrict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+
+let arr = ['plum.png', 'orange.jpg'] */
+/* 
+const result = confirm("Are you here?");
+console.log(result); */
+
+/* const answer = prompt("Вам есть 18,", "18");
+console.log(answer); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша Фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers); */
+
+ console.log(2*4 == 8);
