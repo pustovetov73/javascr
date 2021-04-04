b5973a7aeaeef839c01c89d63977cc5f79f2628d
Vladimir <pustovetov73@gmail.com>
2021-04-04 16:41:36 +0300

    1 pracyica

--- js/script.js
+++ js/script.js
@@ -1,42 +1,19 @@
-"use sctrict";
+const numberOfFilms = +prompt('сколько фильмов ві уже посмотрели?', '');
 
-/* let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-console.log(number);
-
-const obj = {
-    a: 50
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-obj.a = 10;
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(obj);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
-
-console.log(obj.name);
-
-
-let arr = ['plum.png', 'orange.jpg'] */
-/* 
-const result = confirm("Are you here?");
-console.log(result); */
-
-/* const answer = prompt("Вам есть 18,", "18");
-console.log(answer); */
-
-/* const answers = [];
-
-answers[0] = prompt('Как Ваше имя?', '');
-answers[1] = prompt('Как Ваша Фамилия?', '');
-answers[2] = prompt('Сколько Вам лет?', '');
-
-document.write(answers); */
-
- console.log(2*4 == 8);
+console.log(personalMovieDB);
